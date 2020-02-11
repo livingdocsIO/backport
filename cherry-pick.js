44832f58ae298c9b2851bb61e7d48a18af7766cb
Marc Bachmann <marc.brookman@gmail.com>
2020-02-11 12:34:53 +0100

    fix: Also always fetch the master (base) branch of a pull request

--- cherry-pick.js
+++ cherry-pick.js
@@ -48,7 +48,7 @@ module.exports = async function (context, pr, targetBase, token) {
 
   try {
     // Fetch and create branch
-    await worktree.git(['fetch', 'origin', targetBase, pr.head.sha])
+    await worktree.git(['fetch', 'origin', targetBase, pr.head.sha, pr.base.sha])
     await worktree.git(['branch', targetDir, pr.head.sha])
     await worktree.git(['worktree', 'add', targetDir, targetDir])
 
