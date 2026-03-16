229b76c2c1d16339b1aca323cb7539899700cab9
Marc Bachmann <marc.brookman@gmail.com>
2026-03-16 11:59:25 +0100

    fix: Configure http postBuffer

--- cherry-pick.js
+++ cherry-pick.js
@@ -3,13 +3,17 @@ const tmp = require('os').tmpdir()
 const fs = require('fs-extra')
 const execFile = require('util').promisify(require('child_process').execFile)
 
-function createGit (dir, context) {
-  function exec (file, args = [], options) {
+function createGit(dir, context) {
+  function exec(file, args = [], options) {
     context.log.debug(file, ...args)
     return execFile(file, args, {cwd: dir, ...options})
   }
-  function git (args, options) { return exec('/usr/bin/git', args, options) }
-  function child (subdir) { return createGit(path.join(dir, subdir), context) }
+  function git(args, options) {
+    return exec('/usr/bin/git', args, options)
+  }
+  function child(subdir) {
+    return createGit(path.join(dir, subdir), context)
+  }
   exec.git = git
   exec.child = child
   exec.exec = exec
@@ -17,23 +21,37 @@ function createGit (dir, context) {
   return exec
 }
 
-async function getWorktree (slug, context, token) {
+async function getWorktree(slug, context, token) {
   const dir = path.join(tmp, 'backport', slug)
   const worktree = createGit(dir, context)
   try {
     await fs.stat(path.join(worktree.path, '.git'))
-    await worktree.git(['remote', 'set-url', 'origin', `https://x-access-token:${token}@github.com/${slug}.git`])
+    await worktree.git([
+      'remote',
+      'set-url',
+      'origin',
+      `https://x-access-token:${token}@github.com/${slug}.git`
+    ])
   } catch (err) {
     if (err.code !== 'ENOENT') throw err
     await fs.mkdir(worktree.path, {recursive: true})
-    await worktree.git(['clone', '--bare', `https://x-access-token:${token}@github.com/${slug}.git`, '.git'])
+    await worktree.git([
+      'clone',
+      '--bare',
+      `https://x-access-token:${token}@github.com/${slug}.git`,
+      '.git'
+    ])
     await worktree.git(['update-ref', '--no-deref', 'HEAD', 'HEAD^{commit}'])
-    await fs.appendFile(path.join(worktree.path, '.git/config'), '        fetch = +refs/heads/*:refs/remotes/origin/*')
+    await fs.appendFile(
+      path.join(worktree.path, '.git/config'),
+      '        fetch = +refs/heads/*:refs/remotes/origin/*'
+    )
 
     // Setup config
     await worktree.git(['config', '--local', 'user.email', 'dev@livingdocs.io'])
     await worktree.git(['config', '--local', 'user.name', 'Machine User'])
     await worktree.git(['config', '--local', 'commit.gpgsign', 'false'])
+    await worktree.git(['config', '--local', 'http.postBuffer', '524288000'])
   }
 
   return worktree
